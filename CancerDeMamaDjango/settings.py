369a925952fca97cc752692bc19f4c496f774e85
Santym <santimarcelofierrod@gmail.com>
2023-12-12 21:00:10 -0500

    Update DEBUG setting to True in settings.py

--- CancerDeMamaDjango/settings.py
+++ CancerDeMamaDjango/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-#fc5benwnpe5m+mu1!@d8bvp^o1rqlau1871ky!(6_c+6$l)k6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
